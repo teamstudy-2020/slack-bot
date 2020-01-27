634f361d98df6c9f928791225d822f97b9e7f54e
Youngjin-KimY <11kimyj@gmail.com>
2020-01-27 21:43:49 +0900

    flask내용 삭제

--- starterbot.py
+++ starterbot.py
@@ -4,28 +4,16 @@ from flask import Flask
 from flask import Response
 import time
 import requests
-import sys
-import json
 
-
-
-
-
-SLACK_BOT_TOKEN = "xoxb-909535567570-910859498035-2z86DdZnKv9D18Pb8rP2pNmy"
+SLACK_BOT_TOKEN = ""
 DARK_API_KEY = "2e23fdad6dbf4d46df180135e48210c5"
 BOT_ID = "USA56L8H2"
-app = Flask(__name__)
-
-# @RTMClient.run_on(event='message')
-# def say_hello(**payload):
-
 
 @RTMClient.run_on(event='message')
-@app.route('/')
-def hello_world(**payload):
+def say_hello(**payload):
     data = payload['data']
     web_client = payload['web_client']
-    rtm_client = payload['rtm_client']
+    #rtm_client = payload['rtm_client']
     if 'Hello' in data.get('text', []):
         channel_id = data['channel']
         thread_ts = data['ts']
@@ -36,19 +24,22 @@ def hello_world(**payload):
             text=f"Hi <@{user}>!",
             thread_ts=thread_ts
         )
+
     elif 'weather' in data.get('text',[]):
         channel_id = data['channel']
         thread_ts = data['ts']
-        user = data['user']
+        user = data.get('user')
 
-        web_client.chat_postMessage(
-            channel=channel_id,
-            text=f"Hi <@{user}> today is rainy",
-            thread_ts=thread_ts
-        )
-    return 'Hello World!'
+        if user!=None:
+            weatherInfo = weather()
+            web_client.chat_postMessage(
+                channel=channel_id,
+                text=f"Hi <@{user}> today weather at "+weatherInfo['location']+ " is "+weatherInfo['summary'],
+                thread_ts=thread_ts
+            )
 
-@app.route('/weather')
+
+
 def weather():
     lat = 37.516252
     vlong = 127.023549
@@ -62,12 +53,16 @@ def weather():
     ret['summary'] = a['summary']
     ret['apparentTemperature'] = a['apparentTemperature']
     ret['temperature'] = a['temperature']
-    ret = json.dumps(ret).encode('utf8')
-    return Response(ret,content_type='application/json; charset=utf-8')
+    #ret = json.dumps(ret).encode('utf8')
+    #return Response(ret,content_type='application/json; charset=utf-8')
+    return ret
+
 
 if __name__ == "__main__":
-    slack_client = RTMClient(token=SLACK_BOT_TOKEN)
+    slack_client = RTMClient(
+        token=SLACK_BOT_TOKEN,
+        connect_method= 'rtm.start'
+    )
     slack_client.start()
-    app.run()
 
 
