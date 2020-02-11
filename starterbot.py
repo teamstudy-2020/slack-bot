17c88983fd89ee5962a793507c97b9cac511dff2
Youngjin-KimY <11kimyj@gmail.com>
2020-02-11 20:29:49 +0900

    adding information of exchange rate

--- starterbot.py
+++ starterbot.py
@@ -1,13 +1,34 @@
 # -*-coding: UTF-8 -*-
 from slack import RTMClient
-from flask import Flask
-from flask import Response
 import time
+from datetime import date,timedelta
 import requests
-
-SLACK_BOT_TOKEN = ""
+# SLACK_BOT_TOKEN = "xoxb-45215842257-914671033057-OE22GNVZvKAyYRtkmcp8fewG"
+SLACK_BOT_TOKEN = "xoxb-909535567570-910859498035-HfiIg4FUGSNoWlEaPrG2QTKs"
+EXCHANGE_RATE_TOKEN = "SpmyBvns5mrR0RfOX1ibVzbkD7e5hT7A"
 DARK_API_KEY = "2e23fdad6dbf4d46df180135e48210c5"
 BOT_ID = "USA56L8H2"
+
+# Time
+TODAY = date.today()
+YESTERDAY = date.today() - timedelta(days=1)
+TWODAYBEFORE = date.today() - timedelta(days=2)
+
+class exchangeEntity:
+    def __init__(self,ttb,tts,base,unit,nation):
+        self.nation = nation
+        self.ttb = ttb
+        self.tts = tts
+        self.base = base
+        self.unit = unit
+
+    def __str__(self):
+        str = ""
+        str += "국가: "+self.nation+"\t통화코드: "+self.unit+"\t받으실 때: "+self.ttb+"원\t보내실 때: "\
+               +self.tts+"원\t매매 기준: "+self.base + "원\n"
+        return str
+
+
 
 @RTMClient.run_on(event='message')
 def say_hello(**payload):
@@ -30,15 +51,23 @@ def say_hello(**payload):
         thread_ts = data['ts']
         user = data.get('user')
 
-        if user!=None:
-            weatherInfo = weather()
-            web_client.chat_postMessage(
-                channel=channel_id,
-                text=f"Hi <@{user}> today weather at "+weatherInfo['location']+ " is "+weatherInfo['summary'],
-                thread_ts=thread_ts
-            )
+        #if user!=None:
+        weatherInfo = weather()
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Hi <@{user}> today weatther at "+weatherInfo['location']+ " is "+weatherInfo['summary']
+        )
+    elif 'Exchange' in data.get('text',[]):
+        channel_id = data['channel']
+        thread_ts = data['ts']
+        user = data.get('user')
 
 
+        exchage = exchageRate()
+        web_client.chat_postMessage(
+            channel=channel_id,
+            text=f"Hi <@{user}> 물어보신 환율 정보 is 해당 통화 1 단위에 해당하는 한국 원화"+"\n"+exchage
+        )
 
 def weather():
     lat = 37.516252
@@ -53,9 +82,60 @@ def weather():
     ret['summary'] = a['summary']
     ret['apparentTemperature'] = a['apparentTemperature']
     ret['temperature'] = a['temperature']
-    #ret = json.dumps(ret).encode('utf8')
-    #return Response(ret,content_type='application/json; charset=utf-8')
+
     return ret
+
+def exchageRageDataProcessing(dataList):
+    ret = {}
+    for x in dataList:
+        unit = x["cur_unit"]
+        if unit == "CNH":
+            exchange = exchangeEntity(x["ttb"],x["tts"],x["deal_bas_r"],x["cur_unit"],"중국")
+            ret["CNH"] = exchange
+        elif unit == "EUR":
+            exchange = exchangeEntity(x["ttb"], x["tts"], x["deal_bas_r"], x["cur_unit"],"유럽연합")
+            ret["EUR"] = exchange
+        elif unit == "GBP":
+            exchange = exchangeEntity(x["ttb"], x["tts"], x["deal_bas_r"], x["cur_unit"],"영국")
+            ret["GBP"] = exchange
+        elif unit == "USD":
+            exchange = exchangeEntity(x["ttb"], x["tts"], x["deal_bas_r"], x["cur_unit"],"미국")
+            ret["USD"] = exchange
+        elif unit == "HKD":
+            exchange = exchangeEntity(x["ttb"], x["tts"], x["deal_bas_r"], x["cur_unit"],"홍콩")
+            ret["HKD"] = exchange
+
+    return ret
+
+def exchageRate():
+    url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={EXCHANGE_RATE_TOKEN}&searchdate={date}&data=AP01'
+
+    ret = {}
+    toString = ""
+    url = url.format(EXCHANGE_RATE_TOKEN=EXCHANGE_RATE_TOKEN,date=TODAY)
+    res = requests.get(url).json()
+    if res != None:
+        ret = exchageRageDataProcessing(res)
+        for key in ret:
+            toString += ret[key].__str__()+" "
+        return toString
+
+    url = url.format(EXCHANGE_RATE_TOKEN=EXCHANGE_RATE_TOKEN, date=YESTERDAY)
+    res = requests.get(url).json()
+    if res != None:
+        ret = exchageRageDataProcessing(res)
+        for key in ret:
+            toString += ret[key].__str__() + " "
+        return toString
+
+    url = url.format(EXCHANGE_RATE_TOKEN=EXCHANGE_RATE_TOKEN, date=TWODAYBEFORE)
+    res = requests.get(url).json()
+    if res != None:
+        ret = exchageRageDataProcessing(res)
+        for key in ret:
+            toString += ret[key].__str__() + " "
+        return toString
+
 
 
 if __name__ == "__main__":
